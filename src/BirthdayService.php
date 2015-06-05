31710e6c728f7f1704a4dbbe0de266800f54505c
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-06-05 17:27:40 +0200

    Extract method over the code that extracts employees

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -9,20 +9,7 @@ class BirthdayService
 
     public function sendGreetings($fileName, XDate $xDate, $smtpHost, $smtpPort)
     {
-        $fileHandler = fopen($fileName, 'r');
-        fgetcsv($fileHandler);
-
-        $employees = [];
-        
-        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
-            $employeeData = array_map('trim', $employeeData);
-            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-            if ($employee->isBirthday($xDate)) {
-                $employees[] = $employee;
-            }
-        }
-
-        fclose($fileHandler);
+        $employees = $this->findAllWhoseBirthdayIs($fileName, $xDate);
 
         foreach ($employees as $employee) {
             $recipient = $employee->getEmail();
@@ -53,5 +40,25 @@ class BirthdayService
     protected function doSendMessage(Swift_Message $msg)
     {
         $this->mailer->send($msg);
+    }
+
+    private function findAllWhoseBirthdayIs($fileName, XDate $xDate)
+    {
+        $fileHandler = fopen($fileName, 'r');
+        fgetcsv($fileHandler);
+
+        $employees = [];
+
+        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
+            $employeeData = array_map('trim', $employeeData);
+            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+            if ($employee->isBirthday($xDate)) {
+                $employees[] = $employee;
+            }
+        }
+
+        fclose($fileHandler);
+        
+        return $employees;
     }
 }
