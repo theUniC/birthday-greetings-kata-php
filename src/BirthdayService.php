b89f2cccd1ac4cf71b086ea71e5d63bfaf22c864
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-06-05 17:30:41 +0200

    Extract class over the method findAllWhoseBirthdayIs

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -1,5 +1,38 @@
 <?php
 
+class CsvEmployeeRepository
+{
+    /**
+     * @var string
+     */
+    private $fileName;
+
+    public function __construct($fileName)
+    {
+        $this->fileName = $fileName;
+    }
+
+    public function findAllWhoseBirthdayIs(XDate $xDate)
+    {
+        $fileHandler = fopen($this->fileName, 'r');
+        fgetcsv($fileHandler);
+
+        $employees = [];
+
+        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
+            $employeeData = array_map('trim', $employeeData);
+            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+            if ($employee->isBirthday($xDate)) {
+                $employees[] = $employee;
+            }
+        }
+
+        fclose($fileHandler);
+
+        return $employees;
+    }
+}
+
 class BirthdayService
 {
     /**
@@ -44,21 +77,6 @@ class BirthdayService
 
     private function findAllWhoseBirthdayIs($fileName, XDate $xDate)
     {
-        $fileHandler = fopen($fileName, 'r');
-        fgetcsv($fileHandler);
-
-        $employees = [];
-
-        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
-            $employeeData = array_map('trim', $employeeData);
-            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-            if ($employee->isBirthday($xDate)) {
-                $employees[] = $employee;
-            }
-        }
-
-        fclose($fileHandler);
-        
-        return $employees;
+        return (new CsvEmployeeRepository($fileName))->findAllWhoseBirthdayIs($xDate);
     }
 }
